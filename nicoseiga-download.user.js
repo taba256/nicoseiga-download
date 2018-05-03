7475cd5a745e7708c58220acea82564e9db38ecd
taba <taba256@github.com>
2018-05-03 12:22:19 +0900

    ダウンロードの進捗状況表示
    updateURL, downloadURLの追加

--- nicoseiga-download.user.js
+++ nicoseiga-download.user.js
@@ -3,8 +3,10 @@
 // @namespace   https://github.com/taba256/nicoseiga-download
 // @description ニコニコ静画(マンガ)の作品を、zipファイルに圧縮してダウンロードできます。
 // @author      taba
-// @version     1.0.0
+// @version     1.1.0
 // @supportURL  https://github.com/taba256/nicoseiga-download/issues
+// @updateURL   https://github.com/taba256/nicoseiga-download/raw/master/nicoseiga-download.meta.js
+// @downloadURL https://github.com/taba256/nicoseiga-download/raw/master/nicoseiga-download.user.js
 // @match       *://seiga.nicovideo.jp/watch/mg*
 // @grant       unsafeWindow
 // @grant       GM_registerMenuCommand
@@ -18,12 +20,96 @@
 
 (()=>{
 	"use strict";
+	class DownloadItem{
+		constructor(dlinfo,parent_div,name){
+			this.dlinfo=dlinfo;
+			const div=document.createElement("div");
+			div.style.display="flex";
+			const span=document.createElement("span");
+			span.textContent=name;
+			span.style.flex="1";
+			div.appendChild(span);
+			this.progressBar=document.createElement("progress");
+			this.progressBar.max=100;
+			this.progressBar.value=0;
+			this.progressBar.style.flex="2";
+			div.appendChild(this.progressBar);
+			parent_div.appendChild(div);
+			this.completed=false;
+		}
+		update(progress){
+			if(progress.lengthComputable){
+				this.progressBar.max=progress.total;
+				this.progressBar.value=progress.loaded;
+			}
+		}
+		downloadComplete(){
+			this.completed=true;
+			this.dlinfo.checkAllCompleted();
+		}
+		get isCompleted(){
+			return this.completed;
+		}
+	}
+	class DownloadInfomation{
+		constructor(title){
+			this.downloads=[];
+			this.cancel=[];
+			this.fulldiv=document.createElement("div");
+			this.fulldiv.style.cssText="background-color:#ffffff7f;z-index:114514;position:fixed;width:100vw;height:100vh;top:0;left:0;";
+			document.body.appendChild(this.fulldiv);
+			const h1Title=document.createElement("h1");
+			h1Title.textContent=title;
+			h1Title.style.cssText="text-align:center;padding-top:10px;";
+			this.fulldiv.appendChild(h1Title);
+			this.divInfo=document.createElement("div");
+			this.divInfo.style.cssText="width:80%;height:80%;border:dotted medium black;margin:20px auto;background-color:white;overflow:auto;";
+			this.fulldiv.appendChild(this.divInfo);
+			this.buttonOK=document.createElement("button");
+			this.buttonOK.textContent="OK!";
+			this.buttonOK.disabled=true;
+			this.buttonOK.style.cssText="padding:10px 20px;border-radius:20px;";
+			this.buttonOK.addEventListener("click",()=>{
+				this.Dispose();
+			});
+			const buttonCancel=document.createElement("button");
+			buttonCancel.textContent="Cancel";
+			buttonCancel.style.cssText="padding:10px 20px;border-radius:20px;";
+			buttonCancel.addEventListener("click",()=>{
+				this.cancel.forEach(f=>f());
+				this.Dispose();
+			});
+			const buttonDiv=document.createElement("div");
+			buttonDiv.appendChild(this.buttonOK);
+			buttonDiv.appendChild(buttonCancel);
+			buttonDiv.style.cssText="text-align:center;";
+			this.fulldiv.appendChild(buttonDiv);
+		}
+		Dispose(){
+			document.body.removeChild(this.fulldiv);
+		}
+		addDownload(name,cancel){
+			const newItem=new DownloadItem(this,this.divInfo,name);
+			this.downloads.push(newItem);
+			this.cancel.push(cancel);
+			return newItem;
+		}
+		checkAllCompleted(){
+			if(this.downloads.every(d=>d.isCompleted)){
+				this.buttonOK.disabled=false;
+			}
+		}
+	}
+
 	const download=async()=>{
 		try{
 			const zip = new JSZip();
 			const dir = zip.folder(episode_title);
 			const args = unsafeWindow.args;
+			const di=new DownloadInfomation(episode_title);
+
 			await Promise.all(args.pages.map(page=>new Promise((resolve,reject)=>{
+				const dl=di.addDownload((new URL(page.url)).pathname.replace(/.*\//,""),reject);
 				GM_xmlhttpRequest({method:"GET",url:page.url,responseType:"arraybuffer",onload:xhr=>{
 					const url=new URL(xhr.finalUrl);
 					let data=new Uint8Array(xhr.response);
@@ -37,11 +123,15 @@
 					}
 					dir.file(page.image_id+".jpg",data);
 					resolve();
-				},onerror:reject});
+					dl.downloadComplete();
+				},
+				onerror:reject,
+				onprogress:progress=>{dl.update(progress);}});
 			})));
 			const content = await zip.generateAsync({type:"blob",compression:"DEFLATE",compressionOptions:{level:9}})
 			saveAs(content,episode_title+".zip");
 		}catch(e){
+			console.log(e);
 			alert("ダウンロードに失敗しました");
 		}
 	};
