8a1336104fc9112156ea09754c62d77673c3fe1c
taba <taba256@github.com>
2018-05-03 12:22:19 +0900

    ダウンロードの進捗状況表示
    updateURL, downloadURLの追加

---
+++ nicoseiga-download.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        ニコニコ静画(マンガ)ダウンロード
+// @namespace   https://github.com/taba256/nicoseiga-download
+// @description ニコニコ静画(マンガ)の作品を、zipファイルに圧縮してダウンロードできます。
+// @author      taba
+// @version     1.1.0
+// @supportURL  https://github.com/taba256/nicoseiga-download/issues
+// @updateURL   https://github.com/taba256/nicoseiga-download/raw/master/nicoseiga-download.meta.js
+// @downloadURL https://github.com/taba256/nicoseiga-download/raw/master/nicoseiga-download.user.js
+// @match       *://seiga.nicovideo.jp/watch/mg*
+// @grant       unsafeWindow
+// @grant       GM_registerMenuCommand
+// @grant       GM_xmlhttpRequest
+// @connect     drm.nicoseiga.jp
+// @connect     lohas.nicoseiga.jp
+// @connect     seiga.nicovideo.jp
+// @require     https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js
+// @require     https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.min.js
+// ==/UserScript==
