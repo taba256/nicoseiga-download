266604366046ce4081282c05323aa6898d366df3
taba <taba256@github.com>
2020-06-14 15:52:42 +0900

    サーバURLの変更に対応（ユーザ作品）

--- nicoseiga-download.user.js
+++ nicoseiga-download.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/taba256/nicoseiga-download
 // @description ニコニコ静画(マンガ)の作品を、zipファイルに圧縮してダウンロードできます。
 // @author      taba
-// @version     1.1.1
+// @version     1.1.2
 // @supportURL  https://github.com/taba256/nicoseiga-download/issues
 // @updateURL   https://github.com/taba256/nicoseiga-download/raw/master/nicoseiga-download.meta.js
 // @downloadURL https://github.com/taba256/nicoseiga-download/raw/master/nicoseiga-download.user.js
@@ -15,6 +15,7 @@
 // @connect     lohas.nicoseiga.jp
 // @connect     seiga.nicovideo.jp
 // @connect     nicoseiga.cdn.nimg.jp
+// @connect     dcdn.cdn.nimg.jp
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.min.js
 // ==/UserScript==
