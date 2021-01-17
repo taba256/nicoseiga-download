92f94d2a921f9775afe80f30f7f85a738a48a8a0
taba <taba256@users.noreply.github.com>
2021-01-17 11:46:28 +0900

    バージョン番号更新 1.1.4

--- nicoseiga-download.meta.js
+++ nicoseiga-download.meta.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/taba256/nicoseiga-download
 // @description ニコニコ静画(マンガ)の作品を、zipファイルに圧縮してダウンロードできます。
 // @author      taba
-// @version     1.1.3
+// @version     1.1.4
 // @supportURL  https://github.com/taba256/nicoseiga-download/issues
 // @updateURL   https://github.com/taba256/nicoseiga-download/raw/master/nicoseiga-download.meta.js
 // @downloadURL https://github.com/taba256/nicoseiga-download/raw/master/nicoseiga-download.user.js
@@ -18,6 +18,7 @@
 // @connect     dcdn.cdn.nimg.jp
 // @connect     manga-drm.nicoseiga.jp
 // @connect     manga-deliver.nicoseiga.jp
+// @connect     drm.cdn.nicomanga.jp
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.min.js
 // ==/UserScript==
