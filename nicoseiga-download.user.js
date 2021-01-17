6963cbbe3269654c66a34a4121d56cfa6051c396
taba <taba256@users.noreply.github.com>
2021-01-17 11:43:54 +0900

    ソースコードのフォーマット

--- nicoseiga-download.user.js
+++ nicoseiga-download.user.js
@@ -23,128 +23,130 @@
 // @require     https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.min.js
 // ==/UserScript==
 
-(()=>{
+(() => {
 	"use strict";
-	class DownloadItem{
-		constructor(dlinfo,parent_div,name){
-			this.dlinfo=dlinfo;
-			const div=document.createElement("div");
-			div.style.display="flex";
-			const span=document.createElement("span");
-			span.textContent=name;
-			span.style.flex="1";
+	class DownloadItem {
+		constructor(dlinfo, parent_div, name) {
+			this.dlinfo = dlinfo;
+			const div = document.createElement("div");
+			div.style.display = "flex";
+			const span = document.createElement("span");
+			span.textContent = name;
+			span.style.flex = "1";
 			div.appendChild(span);
-			this.progressBar=document.createElement("progress");
-			this.progressBar.max=100;
-			this.progressBar.value=0;
-			this.progressBar.style.flex="2";
+			this.progressBar = document.createElement("progress");
+			this.progressBar.max = 100;
+			this.progressBar.value = 0;
+			this.progressBar.style.flex = "2";
 			div.appendChild(this.progressBar);
 			parent_div.appendChild(div);
-			this.completed=false;
+			this.completed = false;
 		}
-		update(progress){
-			if(progress.lengthComputable){
-				this.progressBar.max=progress.total;
-				this.progressBar.value=progress.loaded;
+		update(progress) {
+			if (progress.lengthComputable) {
+				this.progressBar.max = progress.total;
+				this.progressBar.value = progress.loaded;
 			}
 		}
-		downloadComplete(){
-			this.completed=true;
+		downloadComplete() {
+			this.completed = true;
 			this.dlinfo.checkAllCompleted();
 		}
-		get isCompleted(){
+		get isCompleted() {
 			return this.completed;
 		}
 	}
-	class DownloadInfomation{
-		constructor(title){
-			this.downloads=[];
-			this.cancel=[];
-			this.fulldiv=document.createElement("div");
-			this.fulldiv.style.cssText="background-color:#ffffff7f;z-index:114514;position:fixed;width:100vw;height:100vh;top:0;left:0;";
+	class DownloadInfomation {
+		constructor(title) {
+			this.downloads = [];
+			this.cancel = [];
+			this.fulldiv = document.createElement("div");
+			this.fulldiv.style.cssText = "background-color:#ffffff7f;z-index:114514;position:fixed;width:100vw;height:100vh;top:0;left:0;";
 			document.body.appendChild(this.fulldiv);
-			const h1Title=document.createElement("h1");
-			h1Title.textContent=title;
-			h1Title.style.cssText="text-align:center;padding-top:10px;";
+			const h1Title = document.createElement("h1");
+			h1Title.textContent = title;
+			h1Title.style.cssText = "text-align:center;padding-top:10px;";
 			this.fulldiv.appendChild(h1Title);
-			this.divInfo=document.createElement("div");
-			this.divInfo.style.cssText="width:80%;height:80%;border:dotted medium black;margin:20px auto;background-color:white;overflow:auto;";
+			this.divInfo = document.createElement("div");
+			this.divInfo.style.cssText = "width:80%;height:80%;border:dotted medium black;margin:20px auto;background-color:white;overflow:auto;";
 			this.fulldiv.appendChild(this.divInfo);
-			this.buttonOK=document.createElement("button");
-			this.buttonOK.textContent="OK!";
-			this.buttonOK.disabled=true;
-			this.buttonOK.style.cssText="padding:10px 20px;border-radius:20px;";
-			this.buttonOK.addEventListener("click",()=>{
+			this.buttonOK = document.createElement("button");
+			this.buttonOK.textContent = "OK!";
+			this.buttonOK.disabled = true;
+			this.buttonOK.style.cssText = "padding:10px 20px;border-radius:20px;";
+			this.buttonOK.addEventListener("click", () => {
 				this.Dispose();
 			});
-			const buttonCancel=document.createElement("button");
-			buttonCancel.textContent="Cancel";
-			buttonCancel.style.cssText="padding:10px 20px;border-radius:20px;";
-			buttonCancel.addEventListener("click",()=>{
-				this.cancel.forEach(f=>f());
+			const buttonCancel = document.createElement("button");
+			buttonCancel.textContent = "Cancel";
+			buttonCancel.style.cssText = "padding:10px 20px;border-radius:20px;";
+			buttonCancel.addEventListener("click", () => {
+				this.cancel.forEach(f => f());
 				this.Dispose();
 			});
-			const buttonDiv=document.createElement("div");
+			const buttonDiv = document.createElement("div");
 			buttonDiv.appendChild(this.buttonOK);
 			buttonDiv.appendChild(buttonCancel);
-			buttonDiv.style.cssText="text-align:center;";
+			buttonDiv.style.cssText = "text-align:center;";
 			this.fulldiv.appendChild(buttonDiv);
 		}
-		Dispose(){
+		Dispose() {
 			document.body.removeChild(this.fulldiv);
 		}
-		addDownload(name,cancel){
-			const newItem=new DownloadItem(this,this.divInfo,name);
+		addDownload(name, cancel) {
+			const newItem = new DownloadItem(this, this.divInfo, name);
 			this.downloads.push(newItem);
 			this.cancel.push(cancel);
 			return newItem;
 		}
-		checkAllCompleted(){
-			if(this.downloads.every(d=>d.isCompleted)){
-				this.buttonOK.disabled=false;
+		checkAllCompleted() {
+			if (this.downloads.every(d => d.isCompleted)) {
+				this.buttonOK.disabled = false;
 			}
 		}
 	}
 
-	const download=async()=>{
-		try{
+	const download = async () => {
+		try {
 			const zip = new JSZip();
 			const dir = zip.folder(episode_title);
 			const args = unsafeWindow.args;
-			const di=new DownloadInfomation(episode_title);
+			const di = new DownloadInfomation(episode_title);
 
-			await Promise.all(args.pages.map(page=>new Promise((resolve,reject)=>{
-				const dl=di.addDownload((new URL(page.url)).pathname.replace(/.*\//,""),reject);
-				GM_xmlhttpRequest({method:"GET",url:page.url,responseType:"arraybuffer",onload:xhr=>{
-					const url=new URL(xhr.finalUrl);
-					let data=new Uint8Array(xhr.response);
-					const keystring=xhr.finalUrl.match(/[0-9a-fA-F]{40}/);
-					if(keystring!=null){
-						const key=new Uint8Array(8);
-						for(let i=0;i<8;i++){
-							key[i]=parseInt(keystring[0].substr(i*2,2),16);
+			await Promise.all(args.pages.map(page => new Promise((resolve, reject) => {
+				const dl = di.addDownload((new URL(page.url)).pathname.replace(/.*\//, ""), reject);
+				GM_xmlhttpRequest({
+					method: "GET", url: page.url, responseType: "arraybuffer", onload: xhr => {
+						const url = new URL(xhr.finalUrl);
+						let data = new Uint8Array(xhr.response);
+						const keystring = xhr.finalUrl.match(/[0-9a-fA-F]{40}/);
+						if (keystring != null) {
+							const key = new Uint8Array(8);
+							for (let i = 0; i < 8; i++) {
+								key[i] = parseInt(keystring[0].substr(i * 2, 2), 16);
+							}
+							data = data.map((v, i) => v ^ key[i & 7]);
 						}
-						data=data.map((v,i)=>v^key[i&7]);
-					}
-					dir.file(page.image_id+".jpg",data);
-					resolve();
-					dl.downloadComplete();
-				},
-				onerror:reject,
-				onprogress:progress=>{dl.update(progress);}});
+						dir.file(page.image_id + ".jpg", data);
+						resolve();
+						dl.downloadComplete();
+					},
+					onerror: reject,
+					onprogress: progress => { dl.update(progress); }
+				});
 			})));
-			const content = await zip.generateAsync({type:"blob",compression:"DEFLATE",compressionOptions:{level:9}})
-			saveAs(content,episode_title+".zip");
-		}catch(e){
+			const content = await zip.generateAsync({ type: "blob", compression: "DEFLATE", compressionOptions: { level: 9 } })
+			saveAs(content, episode_title + ".zip");
+		} catch (e) {
 			console.log(e);
 			alert("ダウンロードに失敗しました");
 		}
 	};
-	const title_element=document.querySelector("div.title");
-	const episode_title=title_element.innerText.replace("\n"," ");
+	const title_element = document.querySelector("div.title");
+	const episode_title = title_element.innerText.replace("\n", " ");
 	const downloadButton = document.createElement("button");
 	downloadButton.textContent = "ダウンロード";
-	downloadButton.addEventListener("click",download);
+	downloadButton.addEventListener("click", download);
 	title_element.appendChild(downloadButton);
-	GM_registerMenuCommand("ニコニコ静画ダウンロード",download);
+	GM_registerMenuCommand("ニコニコ静画ダウンロード", download);
 })();
