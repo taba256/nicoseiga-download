e19c9ee3856ed1a282de5a220968bcdb0ebe4f6a
taba <taba256@users.noreply.github.com>
2021-01-28 23:04:11 +0900

    @connectの整理
    @connectはサブドメインも勝手に許可してくれるらしいので採用
    今後のサーバ追加や廃止に対応する手間を省かせて頂く

--- nicoseiga-download.meta.js
+++ nicoseiga-download.meta.js
@@ -11,14 +11,10 @@
 // @grant       unsafeWindow
 // @grant       GM_registerMenuCommand
 // @grant       GM_xmlhttpRequest
-// @connect     drm.nicoseiga.jp
-// @connect     lohas.nicoseiga.jp
-// @connect     seiga.nicovideo.jp
-// @connect     nicoseiga.cdn.nimg.jp
-// @connect     dcdn.cdn.nimg.jp
-// @connect     manga-drm.nicoseiga.jp
-// @connect     manga-deliver.nicoseiga.jp
-// @connect     drm.cdn.nicomanga.jp
+// @connect     nicoseiga.jp
+// @connect     nicovideo.jp
+// @connect     nimg.jp
+// @connect     nicomanga.jp
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.min.js
 // ==/UserScript==
