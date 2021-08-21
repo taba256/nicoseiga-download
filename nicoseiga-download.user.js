fc1d3132c3278f4f7ea56fecc33579cf5cbc35d9
taba <taba256@users.noreply.github.com>
2021-08-22 03:45:37 +0900

    branch名をmainに変更

--- nicoseiga-download.user.js
+++ nicoseiga-download.user.js
@@ -3,10 +3,10 @@
 // @namespace   https://github.com/taba256/nicoseiga-download
 // @description ニコニコ静画(マンガ)の作品を、zipファイルに圧縮してダウンロードできます。
 // @author      taba
-// @version     1.1.5
+// @version     1.1.6
 // @supportURL  https://github.com/taba256/nicoseiga-download/issues
-// @updateURL   https://github.com/taba256/nicoseiga-download/raw/master/nicoseiga-download.meta.js
-// @downloadURL https://github.com/taba256/nicoseiga-download/raw/master/nicoseiga-download.user.js
+// @updateURL   https://github.com/taba256/nicoseiga-download/raw/main/nicoseiga-download.meta.js
+// @downloadURL https://github.com/taba256/nicoseiga-download/raw/main/nicoseiga-download.user.js
 // @match       *://seiga.nicovideo.jp/watch/mg*
 // @grant       unsafeWindow
 // @grant       GM_registerMenuCommand
